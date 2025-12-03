10103f27d3b5c088cbfdceb6c6aca7b8547ad05c
Prayag-R <prayag.s.rana@gmail.com>
2025-12-02 21:08:43 -0500

    prob?

--- my-gemini-backend/server.js
+++ my-gemini-backend/server.js
@@ -28,8 +28,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: '*',
-  credentials: false,
+  origin: 'https://neighborhood-pearl.vercel.app',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
