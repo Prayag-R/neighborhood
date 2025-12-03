f6cf031aa1c6901d40283bbacf0a4c6f8309a779
Prayag-R <prayag.s.rana@gmail.com>
2025-12-02 21:11:06 -0500

    work?

--- my-gemini-backend/server.js
+++ my-gemini-backend/server.js
@@ -28,11 +28,23 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: 'https://neighborhood-pearl.vercel.app',
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://neighborhood-pearl.vercel.app'
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors());
 
 app.use(express.json());
 
